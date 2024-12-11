bef85c01de477f23f024cba93238eb885eda515a
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-12-11 14:22:55 +0900

    Allow stage to be different size than 20x11

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,21 +1,13 @@
 STAGE = <<~STAGE.freeze
-  XXXXXXXXXXXXXXXXXXXX
-  XXXXXXXXXXXXXXXXXXXX
-  XXXXXXXXXXXXXXXXXXXX
-  XXXXXXXXXXXXXXXXXXXX
-  XXXXXXXXXXXXXXXXXXXX
-  XXXXX        XXXXXXX
-  XXXXXXXXXXXXXXXXXXXX
-  XXXXXXXXXXXXXXXXXXXX
-  XXXXXXXXXXXXXXXXXXXX
-  XXXXXXXXXXXXXXXXXXXX
-  XXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXX
+  X        X
+  XXXXXXXXXX
 STAGE
 
 def tick(args)
   args.state.stage ||= prepare_stage(STAGE)
 
-  args.outputs.primitives << args.state.stage.flat_map { |column|
+  args.outputs.primitives << args.state.stage[:cells].flat_map { |column|
     column.map { |cell|
       cell[:sprite]
     }
@@ -25,30 +17,47 @@ def tick(args)
 end
 
 CELL_SIZE = 64
-OFFSET_Y = (720 - (720.idiv(CELL_SIZE) * CELL_SIZE)).idiv(2)
 
 def prepare_stage(stage)
-  cells = stage.split("\n").map(&:chars)
-               .reverse # make y from bottom to top
-               .transpose # make 2d array have x as first index
+  stage_data = stage.split("\n").map(&:chars)
+                    .reverse # make y from bottom to top
+                    .transpose # make 2d array have x as first index
 
-  cells.map_with_index { |column, x|
-    column.map_with_index { |cell, y|
+  columns = stage_data.size
+  rows = stage_data.first.size
+
+  offset_x = (1280 - (columns * CELL_SIZE)).idiv(2)
+  offset_y = (720 - (rows * CELL_SIZE)).idiv(2)
+
+  cells = (0...columns).map { |col|
+    column = stage_data[col]
+    x = (col * CELL_SIZE) + offset_x
+
+    (0...rows).map { |row|
+      cell_type = column[row]
       {
         sprite: {
-          x: x * CELL_SIZE,
-          y: (y * CELL_SIZE) + OFFSET_Y,
+          x: x,
+          y: (row * CELL_SIZE) + offset_y,
           w: CELL_SIZE,
           h: CELL_SIZE,
-          **cell_sprite(cell)
+          **cell_sprite(cell_type)
         }
       }
     }
   }
+
+  {
+    columns: columns,
+    rows: rows,
+    cells: cells,
+    offset_x: offset_x,
+    offset_y: offset_y
+  }
 end
 
-def cell_sprite(cell)
-  case cell
+def cell_sprite(cell_type)
+  case cell_type
   when 'X'
     { path: :pixel, r: 100, g: 100, b: 100 }
   when ' '
