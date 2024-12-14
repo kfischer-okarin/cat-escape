54ff709151d7d489eced8cf3ffab80f66b6f6518
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-12-14 14:39:56 +0900

    Refactor movement code a bit

--- mygame/app/movement.rb
+++ mygame/app/movement.rb
@@ -2,16 +2,18 @@ def try_to_move_cat(stage, cat:, direction:)
   result = []
 
   cat_entity = stage[:cats][cat]
+  cat_position = { x: cat_entity[:x], y: cat_entity[:y] }
   target_x = cat_entity[:x] + direction[:x]
   target_y = cat_entity[:y] + direction[:y]
+  target_position = { x: target_x, y: target_y }
 
   target_cell = stage[:cells][target_x][target_y]
   if target_cell == :wall
     result << {
       type: :cat_bumped_into_wall,
       cat: cat,
-      from: cat_entity.slice(:x, :y),
-      to: { x: target_x, y: target_y }
+      from: cat_position,
+      to: target_position
     }
   else
     box_in_cell = stage[:objects].find { |object|
@@ -21,8 +23,8 @@ def try_to_move_cat(stage, cat:, direction:)
     cat_moved_event = {
       type: :cat_moved,
       cat: cat,
-      from: cat_entity.slice(:x, :y),
-      to: { x: target_x, y: target_y }
+      from: cat_position,
+      to: target_position
     }
 
     if box_in_cell
@@ -31,8 +33,8 @@ def try_to_move_cat(stage, cat:, direction:)
         result << {
           type: :cat_bumped_into_box,
           cat: cat,
-          from: cat_entity.slice(:x, :y),
-          to: { x: target_x, y: target_y }
+          from: cat_position,
+          to: target_position
         }
       else
         result << cat_moved_event
