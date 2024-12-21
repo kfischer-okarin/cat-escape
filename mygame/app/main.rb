6352f7dac4d17f91640ff5ee608ae466746226a8
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-12-21 17:16:12 +0900

    Add credits

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -13,6 +13,13 @@ COLORS = {
   gray: { r: 100, g: 100, b: 100 }
 }.transform_values(&:freeze).freeze
 
+# "Wholesome" Kevin MacLeod (incompetech.com)
+# Licensed under Creative Commons: By Attribution 4.0 License
+# http://creativecommons.org/licenses/by/4.0/
+
+# Meow sounds by freesound_community from Pixabay
+# https://pixabay.com/sound-effects/tom-cat-meow-27989/
+
 CAT_SPRITES = [
   'sprites/cat.png',
   'sprites/tuxedo-cat.png'
